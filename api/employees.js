0d5bf4c4543516c1de4d778d56d23d6cdb0a8f67
Zaki Shaikh <zaki-shaikh@outlook.com>
2022-03-01 01:05:42 +0530

    Updated employees API

--- api/employees.js
+++ api/employees.js
@@ -4,43 +4,36 @@ const getConfigs = require('../data-access/pool-configs')
 
 const router = express.Router();
 const poolConfig = getConfigs()
-const db1 = dataAccess(poolConfig.uk.name)
-const db2 = dataAccess(poolConfig.siberia.name)
+const dbUk = dataAccess(poolConfig.uk.name)
+const dbSiberia = dataAccess(poolConfig.siberia.name)
 
 router.get('/', async (req, res) => {
     try {
-        const result1 = await db1.query(`SELECT * FROM Employee ORDER BY Id DESC`);
-        const employees1 = result1.recordset;
+        const command = `SELECT * FROM Employee ORDER BY Id DESC`
 
-        const result2 = await db2.query(`SELECT * FROM Employee ORDER BY Id DESC`);
-        const employees2 = result2.recordset;
+        const uk = (await dbUk.query(command)).recordset;
+        const siberia = (await dbSiberia.query(command)).recordset;
 
-        res.json({
-            employees1,
-            employees2
-        });
+        res.json({ uk, siberia });
     } catch (error) {
-        console.log(error)
         res.status(500).json(error);
     }
 });
 
 router.get('/:id', async (req, res) => {
     try {
-        const result1 = await db1.query(`SELECT * FROM Employee WHERE Id = @Id`, [
+        const command = `SELECT * FROM Employee WHERE Id = @Id`
+        const inputs = [
             { name: 'Id', value: req.params.id }
-        ]);
-        const employee1 = result1.recordset.length ? result1.recordset[0] : null;
+        ]
 
-        const result2 = await db2.query(`SELECT * FROM Employee WHERE Id = @Id`, [
-            { name: 'Id', value: req.params.id }
-        ]);
-        const employee2 = result2.recordset.length ? result2.recordset[0] : null;
+        const resultUk = await dbUk.query(command, inputs);
+        const [uk = null] = resultUk.recordset;
 
-        res.json({
-            employee1, 
-            employee2
-        })
+        const resultSiberia = await dbSiberia.query(command, inputs);
+        const [siberia = null] = resultSiberia.recordset;
+
+        res.json({ uk, siberia })
     } catch (error) {
         res.status(500).json(error);
     }
